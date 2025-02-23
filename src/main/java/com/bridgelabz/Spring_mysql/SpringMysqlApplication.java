3762eb45b0b142e6678faf68b53094ebf63f1782
Anandsoni992 <anandsoni9165@gmail.com>
2025-02-23 12:45:59 +0530

    Anand Add . H2 databse connectivity

--- src/main/java/com/bridgelabz/Spring_mysql/SpringMysqlApplication.java
+++ src/main/java/com/bridgelabz/Spring_mysql/SpringMysqlApplication.java
@@ -2,6 +2,7 @@ package com.bridgelabz.Spring_mysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class SpringMysqlApplication {
