635ec7ad5121005feb4e6e6e6bdd318728e3b87b
Anandsoni992 <anandsoni9165@gmail.com>
2025-02-27 14:55:52 +0530

    Anand Add . Adding more attributes of employee

--- src/main/java/com/bridgelabz/Spring_mysql/model/Employee.java
+++ src/main/java/com/bridgelabz/Spring_mysql/model/Employee.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -17,20 +20,63 @@ public class Employee {
     private Long id;
 
     private String name;
-    private String department;
+    //private String department;
     private double salary;
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    @ElementCollection
+    private List<String> department;
+
 
     // Constructors
     public Employee() {}
 
-    public Employee(String name, String department, double salary) {
+    public Employee(String name, Double salary, String gender, LocalDate startDate, String note, String profilePic, List<String> department) {
         this.name = name;
-        this.department = department;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
+        this.note = note;
+        this.profilePic = profilePic;
+        this.department = department;
     }
     public Employee(String name, double salary){
         this.name = name ;
         this.salary = salary;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     // Getters and Setters
@@ -40,8 +86,8 @@ public class Employee {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public String getDepartment() { return department; }
-    public void setDepartment(String department) { this.department = department; }
+    public List<String> getDepartment() { return department; }
+    public void setDepartment(List<String> department) { this.department = department; }
 
     public double getSalary() { return salary; }
     public void setSalary(double salary) { this.salary = salary; }
