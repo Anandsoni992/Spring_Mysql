76faf16e9470b89e897e538c420eb8ad843d7dd9
Anandsoni992 <anandsoni9165@gmail.com>
2025-02-27 16:57:15 +0530

    Anand Add . Changed services for update employee attributes

--- src/main/java/com/bridgelabz/Spring_mysql/service/EmployeeService.java
+++ src/main/java/com/bridgelabz/Spring_mysql/service/EmployeeService.java
@@ -29,11 +29,18 @@ public class EmployeeService {
     }
 
     public Employee updateEmployee(Long id, Employee updatedEmployee) {
-        if (repository.existsById(id)) {
-            updatedEmployee.setId(id);
-            return repository.save(updatedEmployee);
-        }
-        return null;
+        return repository.findById(id)
+   .map(employee -> {
+            employee.setName(updatedEmployee.getName());
+            employee.setSalary(updatedEmployee.getSalary());
+            employee.setGender(updatedEmployee.getGender());
+            employee.setStartDate(updatedEmployee.getStartDate());
+            employee.setNote(updatedEmployee.getNote());
+            employee.setProfilePic(updatedEmployee.getProfilePic());
+            employee.setDepartment(updatedEmployee.getDepartment());
+            return repository.save(employee);
+        })
+                .orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
     }
 
     public void deleteEmployee(Long id) {
